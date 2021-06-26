6b158816455b2004c85781440affb504e5f30592
SimonGPort <simon.giroux.portelance@gmail.com>
2021-06-26 11:30:22 -0400

    connect string

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+// Voir tutoriel de Traversy Media https://www.youtube.com/watch?v=TueoFdaJV2g&list=PLillGF-RfqbaEmlPcX5e_ejaK7Y5MydkW&index=8
+
 let express = require('express')
 let app = express()
-let pg=require('pg')
+let {Pool}=require('pg')
+
+let pool=new Pool()
+
+//DB conect string
+// let connect="postgres://username:password@localhost/databaseName";
+let connect="postgres://postgresql-test:Maruchi1@localhost/postgresql-test";
 
 // return res.send(JSON.stringify("hello world" ));
 app.get('/',function(req,res){
-  pg.connect(connect,function(err,client,done){
+    pool.connect(connect,function(err,client,done){
 if(err){
     return console.error('error fetching client fro pool',err)
 }
